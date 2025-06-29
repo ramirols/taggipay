ba1fb46c2f5204efd832fefbf4ac1e2fe3ae2cab
ramirols <ramirols.dev@gmail.com>
2025-06-29 16:17:46 -0500

    :tada: fix .env

--- src/app/lib/supabase/client.ts
+++ src/app/lib/supabase/client.ts
@@ -1,10 +1,12 @@
 
-import { createBrowserClient } from "@supabase/ssr";
+//import { createBrowserClient } from "@supabase/ssr";
+
+import { createClient } from "@supabase/supabase-js";
 
 let _supabase;
 
 if (!_supabase) {
-    _supabase = createBrowserClient(
+    _supabase = createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
--- src/app/lib/supabase/server.ts
+++ src/app/lib/supabase/server.ts
@@ -1,24 +1,31 @@
-import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
+{/*import { cookies } from "next/headers";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
-export const createServerSupabaseClient = async () => {
-  const cookieStore = await cookies();
+export const createServerSupabaseClient = (): SupabaseClient | null => {
+  const url = process.env.SUPABASE_URL;
+  const key = process.env.SUPABASE_ANON_KEY;
 
-  return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name, value, options) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name, options) {
-          cookieStore.set({ name, value: "", ...options });
-        },
+  if (!url || !key) {
+    console.warn("❌ SUPABASE_URL o SUPABASE_ANON_KEY no definidos.");
+    return null;
+  }
+
+  const cookieStore = cookies();
+
+  return createServerClient(url, key, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
       },
-    }
-  );
-};
+      set(name: string, value: string, options: CookieOptions) {
+        // @ts-ignore - next/headers.cookies.set no tipa igual que espera supabase/ssr
+        cookieStore.set({ name, value, ...options });
+      },
+      remove(name: string, options: CookieOptions) {
+        // @ts-ignore
+        cookieStore.set({ name, value: "", ...options });
+      },
+    },
+  });
+};*/}
